e98c9a55072c20a2c8d279f6a4c14c83ba638bc2
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2023-03-29 09:54:15 +0100

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,53 +1,93 @@
-import statistics
+iimport statistics
 
 class Comparison():
     
     def __init__(self, lower, higher, rating) -> None:
         
-        self.data = [((lower, higher), [rating])]
-        self.reach = Reach().add_to_dict(lower, (higher, rating))
+        self.data = [RatedPair(lower, higher, [rating])] 
+        self.reach = Reach().add_to_dict(lower, (higher, rating)) # ?
             
-    def add_comparison(self, comparison):
+    def add_comparison(self, new_rated_pair):
         
-        for element in self.data:
-            if comparison[0] == element[0]:
-                element[1].appennd(comparison[1])
+        for existing_rated_pair in self.data:
+            if new_rated_pair.lower == existing_rated_pair.lower and new_rated_pair.higher == existing_rated_pair.higher:
+                existing_rated_pair.rating.append(new_rated_pair.rating)
                 return
         
-        self.data.append(comparison)
+        self.data.append(new_rated_pair)
         
     def get_higher_lower(self, assignment1, assignment2):
         
         probability = None
-        if not self.data: 
+        
+        # len = 0, Neither assignments in self.data => return probability 50/50
+        if len(self.data) == 0: 
             probability = 0.5
+        
+        # If single element in self.data AND assignemt1 and assignemt2 in self.data => return average of probability.
         if len(self.data) == 1:
-            if assignment1 == self.data[0][0][0] and assignment2 == self.data[0][0][1]:
-                probability = statistics.mean(self.data[0][1])
-            elif assignment2 == self.data[0][0][0] and assignment1 == self.data[0][0][1]:
-                probability = 1 - (statistics.mean(self.data[0][1]))
+            if assignment1 == self.data[0].lower and assignment2 == self.data[0].higher:
+                probability = statistics.mean(self.data[0].rating)
+            elif assignment2 == self.data[0].lower and assignment1 == self.data[0].higher:
+                probability = 1 - (statistics.mean(self.data[0].rating))
+                
         else:
-            a1_lower = [el for el in self.data if assignment1 == el[0][0]]
-            a2_lower = [el for el in self.data if assignment2 == el[0][0]]
-            
-            # Add rating
-            a1_reach = Reach().add_to_dict([el[1] for el in a1_lower])
-            
-            
-            
-        
+            a1_reach = Reach(assignment1, assignment2, self.data)
+            a2_reach = Reach(assignment2, assignment1, self.data)
+                
+                   
         return ((assignment1, assignment2), probability)
-        
-    def tail(self):
-        
-        data = self.data
-        
         
 class Reach():
     
-    def __init__(self) -> None:
-        self.dict = {}
+    def __init__(self, src, dest, data):
         
-    def add_to_dict(self, src, dests):
+        self.src = src
+        self.dest = dest
+        self.connections_up = {} 
+        self.connections_down = {}
+        self.get_connections(data)
         
-        self.dict.setdefault(src, []).append(dests)
+    def get_connections(self, data):
+        
+        for rated_pair in data:
+            if rated_pair.lower == self.src:
+                self.add_to_dict(self.connections_up, rated_pair.higher, rated_pair.rating, data)
+            elif rated_pair.higher == self.src:
+                self.add_to_dict(self.connections_down, rated_pair.lower, rated_pair.rating, data)
+    
+    def add_to_dict(self, dict, src, rating, data):
+        
+        if src in dict:
+            dict[src][1].append(rating)
+        else:
+            dict[src] = [Reach(src, self.dest, data), [rating]]
+            
+    def get_paths(self):
+        
+        for src, data in self.connections_up.items():
+            
+            if src == self.dest:
+                # Path Dest Found
+                pass
+            else:
+                # Extend Path
+                pass
+            
+        for src, data in self.connections_down.items():
+            
+            if src == self.dest:
+                # Path Dest Found
+                pass
+            else:
+                # Extend Path
+                pass
+        
+class RatedPair():
+    
+    def __init__(self, lower, higher, rating):
+        
+        self.lower = lower
+        self.higher = higher
+        self.rating = rating
+        
