d21258ef0c6d127b53d31db06048a9d32b2769fb
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2023-04-09 04:40:52 +0100

    Delete ranking_data.py

--- ranking_data.py
+++
@@ -1,8 +0,0 @@
-class RankingData():
-    
-    def __init__(self):
-        self.comparisons = []
-        self.comparison_scores = {}
-        self.cumulative_ranking = [[],[]]
-        self.extended_cumulative_ranking = [[],[]]
-        self.high_low = None
