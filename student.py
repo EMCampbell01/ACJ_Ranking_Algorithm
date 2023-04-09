6c10f58a2fe1defe25229ebedf9b559a7674dc92
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2023-04-09 04:41:11 +0100

    Delete student.py

--- student.py
+++
@@ -1,9 +0,0 @@
-from ranking_data import RankingData
-
-class Student():
-    
-    def __init__(self, id, review, rating):
-        self.student_id = id
-        self.reviews = review
-        self.rating = rating
-        self.ranking_data = RankingData()
