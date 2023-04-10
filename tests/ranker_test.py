f133714d46a4d2994430d5d417b84c5cd89f0e37
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2023-04-10 17:06:01 +0100

    Update ranker_test.py

--- tests/ranker_test.py
+++ tests/ranker_test.py
@@ -16,8 +16,9 @@ Can add a error probability. 0 = % chance of error, 1 = 100% chance of error
 Students 'correct ranking' is students in an ascending sequence (LOW 1, 2, 3, 4, 5 HIGH)
 '''
 
-STUDENT_COUNT = 20
+STUDENT_COUNT = 25
 ERROR_PROBABILITY = 0.1
+NUM_REVIEWS = 100
 
 class RankerTest():
     
@@ -26,12 +27,38 @@ class RankerTest():
         self.ranker = Ranker()
         self.student_total = student_total
         self.error_probability = error_probabiliy
+        self.real_ranking = list(range(STUDENT_COUNT))
+        random.shuffle(self.real_ranking)
         
-    def test(self):
+    def test(self, num_pairs: int):
         
-        while True:
-            self.add_random_pairs(10)
-            self.display_ranking()
+        for student in range(STUDENT_COUNT):
+            if student == STUDENT_COUNT - 1:
+                ordered_pair = self.get_real_order(0, student)
+                self.ranker.add_new_pair(RatedPair(ordered_pair[0], ordered_pair[1], random.uniform(0.5, 1)))
+            else:
+                ordered_pair = self.get_real_order(student, (student+1))
+                self.ranker.add_new_pair(RatedPair(ordered_pair[0], ordered_pair[1], random.uniform(0.5, 1)))
+        
+        added_pairs = 0
+        while added_pairs < num_pairs:
+            next_best_reviews = self.ranker.get_next_best_reviews()
+            
+            if not next_best_reviews:
+                self.add_random_pairs(1)
+            else:
+                ordered_pair = self.get_real_order(next_best_reviews[0][0], next_best_reviews[0][1])
+                self.ranker.add_new_pair(RatedPair(ordered_pair[0], ordered_pair[1], random.uniform(0.5, 1)))
+                
+            ranking = self.ranker.get_ranking()
+            
+            if self.finished_ranking(ranking):
+                print(f'Completed ranking after only {added_pairs} pairs')
+                break
+            
+            added_pairs += 1
+            
+        self.display_ranking(ranking)    
         
     def add_random_pairs(self, num):
         
@@ -43,22 +70,17 @@ class RankerTest():
             
             
             # If random number within error probabilty, add inverted pair, otherwise add correct pair
+            ordered_pair = self.get_real_order(student1, student2)
             if random.random() > self.error_probability:
-               new_pair = RatedPair(min(student1, student2), max(student1, student2), random.uniform(0.5, 1))
+               new_pair = RatedPair(ordered_pair[0], ordered_pair[1], random.uniform(0.5, 1))
             else:
-                new_pair = RatedPair(max(student1, student2), min(student1, student2), random.uniform(0.5, 1))
+                new_pair = RatedPair(ordered_pair[1], ordered_pair[0], random.uniform(0.5, 1))
                 new_pair.lower, new_pair.higher = new_pair.higher, new_pair.lower
             
             self.ranker.add_new_pair(new_pair)
     
     # Draws primative box plot in output
-    def display_ranking(self):
-        
-        ranking = self.ranker.get_ranking()
-        
-        print("RATED PAIRS:")
-        for rated_pair in self.ranker.data:
-            print(f'{rated_pair.lower}, {rated_pair.higher}')
+    def display_ranking(self, ranking):
             
         print("RANKING:")
         for student, result in ranking.items():
@@ -75,7 +97,26 @@ class RankerTest():
                 row.append("-")
                 
             print(''.join(row))
+            
+    def finished_ranking(self, ranking):
+        
+        if len(ranking.items()) < STUDENT_COUNT:
+            return False
+        
+        for student, result in ranking.items():
+            if result["low"] + 2 != result["high"]:
+                return False
+        
+        return True
+    
+    def get_real_order(self, student1, student2):
+        for student in self.real_ranking:
+            if student == student1:
+                return (student1, student2)
+            if student == student2:
+                return (student2, student1)
    
 if __name__ == "__main__":                       
     tester = RankerTest(STUDENT_COUNT, ERROR_PROBABILITY)
-    tester.test()      
+    print(f'Real ranking: {tester.real_ranking}')
+    tester.test(NUM_REVIEWS)      
