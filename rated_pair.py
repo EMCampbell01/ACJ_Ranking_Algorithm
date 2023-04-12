6005046a2cc3f00e4a0e323b3ccec9ca25e8f336
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2023-04-12 09:01:14 +0100

    Update rated_pair.py

--- rated_pair.py
+++ rated_pair.py
@@ -1,10 +1,17 @@
 class RatedPair():
     
+    index = 0
+    
     def __init__(self, lower: int, higher: int, rating):
         
         self.lower = lower
         self.higher = higher
         self.rating = rating
+        self.reviewed = False
+        self.number = self.index
+        self.index += 1
+        
         
     def square_rating(self):
+        
         self.rating = self.rating ** 2
